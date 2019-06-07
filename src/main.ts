134d9666c53e0b4abf50b27cfbc6ad6c60ad7a1c
Louis Orleans <louis@orleans.io>
2019-06-06 19:21:33 -0700

    ✨⚡️🔒 add helmet & compression

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import * as compression from 'compression';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = (await NestFactory.create(AppModule))
+    .use(compression())
+    .use(helmet());
+
   await app.listen(3000);
 }
 bootstrap();
