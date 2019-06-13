dc1efc50341399adb1a5391caab17b484470517e
Louis Orleans <louis@orleans.io>
2019-06-13 16:41:52 -0600

    ✨🔒 fix token expiration & improve errors

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,10 +5,11 @@ import {
   Logger,
   UnauthorizedException,
 } from '@nestjs/common';
+import { DateTime } from 'luxon';
+import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
 
 import { User, UserService } from '../user/';
-import { UserTokenService } from '../user-token/';
-import { DateTime } from 'luxon';
+import { UserTokenService, UserToken } from '../user-token/';
 
 @Injectable()
 export class AuthService {
@@ -23,18 +24,24 @@ export class AuthService {
     // Validate if token passed along with HTTP request
     // is associated with any registered account in the database
     // return await this.userService.findOneByToken(token);
-    const user_token = await this.userTokenService.findOneByToken(token);
+    const user_token = await this.userTokenService
+      .findOneByToken(token)
+      .catch(err => {
+        if (err instanceof EntityNotFoundError) {
+          throw new UnauthorizedException('Token invalid');
+        } else {
+          throw err;
+        }
+      });
 
     // check if token is expired
-    if (
-      user_token.created_at.plus(user_token.ttl.toDuration()) < DateTime.local()
-    ) {
+    if (user_token.created_at.plus(user_token.ttl) < DateTime.local()) {
       // delete token from DB
       await this.userTokenService.deleteToken(user_token.authorization_token);
 
       // throw expired token error
       // TODO: figure out a more appropriate error type
-      throw new Error('token expired');
+      throw new UnauthorizedException('Token expired');
     } else {
       return user_token.user;
     }
