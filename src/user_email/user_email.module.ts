d5a94db2c54199769b441a4df6e47c731135ef6b
Josh Gibbs <upaymeifixit@gmail.com>
2019-06-26 14:34:43 -0700

    🐛 added AuthModule to UserEmailModule imports

--- src/user_email/user_email.module.ts
+++ src/user_email/user_email.module.ts
@@ -1,11 +1,16 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UserEmailService } from './user_email.service';
 import { UserEmail } from './user_email.entity';
+import { AuthModule, passportModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEmail])],
+  imports: [
+    TypeOrmModule.forFeature([UserEmail]),
+    passportModule,
+    forwardRef(() => AuthModule),
+  ],
   providers: [UserEmailService],
   exports: [UserEmailService],
 })
