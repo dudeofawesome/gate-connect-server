8ebf38410bb1aa5cae2e6e4cfeed6dd514a4cd55
Louis Orleans <louis@orleans.io>
2019-06-26 06:17:03 -0700

    ✨🚨 add rule against barreling

--- eslint/index.js
+++ eslint/index.js
@@ -5,5 +5,6 @@ module.exports = {
     'no-findOne-by-id': require('./no-findOne-by-id'),
     'disallow-src-dist-import': require('./disallow-src-dist-import'),
     'param-decorator-no-leading-colon': require('./param-decorator-no-leading-colon'),
+    'disable-barreling': require('./disable-barreling'),
   },
 };
