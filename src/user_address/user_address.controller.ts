90a6350d352c73634b5e071486ea0d1e4056cb7c
Josh Gibbs <upaymeifixit@gmail.com>
2019-06-26 13:05:51 -0700

    ✨ creating user_address.controller

--- src/user_address/user_address.controller.ts
+++ src/user_address/user_address.controller.ts
@@ -1,3 +1,12 @@
+import {
+  Controller,
+} from '@nestjs/common';
+import { UserAddressService } from './user_address.service';
 import { UserAddressInfoGuard } from '../utils/guards/user-address-info.guard';
+
+@Controller('user_addresses')
+export class UserAddressController {
+  constructor(private readonly user_address_service: UserAddressService) {}
   @UseGuards(AuthGuard(), UserAddressInfoGuard) // TODO: Create UserAddressInfoGuard to make sure user can't change write only information
   @UseGuards(AuthGuard(), UserAddressInfoGuard)
+}
