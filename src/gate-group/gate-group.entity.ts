f91533e46bfe13be4776030d5bf52545ac5951df
Josh Gibbs <upaymeifixit@gmail.com>
2019-06-26 12:50:04 -0700

    🐛 fixing unbarreling bug

--- src/gate-group/gate-group.entity.ts
+++ src/gate-group/gate-group.entity.ts
@@ -17,7 +17,7 @@ import {
 } from '../utils/transformers/';
 import { Gate } from '../gate/gate.entity';
 import { GateGroupOwner } from '../gate-group-owner/gate-group-owner.entity';
-import { GateGroupAddress } from '../gate_group_address';
+import { GateGroupAddress } from '../gate_group_address/gate_group_address.entity';
 
 @Entity()
 export class GateGroup {
