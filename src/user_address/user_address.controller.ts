09fa3a060afcbd3338772810e94b9c58038821e0
Josh Gibbs <upaymeifixit@gmail.com>
2019-06-26 13:14:00 -0700

    ✨ created user_address GET endpoint

--- src/user_address/user_address.controller.ts
+++ src/user_address/user_address.controller.ts
@@ -1,5 +1,6 @@
 import {
   Controller,
+  Get,
   Post,
   Body,
   UseInterceptors,
@@ -14,6 +15,7 @@ import {
 } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+import { UserAccess } from '../utils/guards/user-access.guard';
 import { UserAddress } from '../user_address/user_address.entity';
 import { UserAddressService } from './user_address.service';
 import { UserParam } from '../utils/decorators/user.param.decorator';
@@ -23,7 +25,22 @@ import { UserAddressInfoGuard } from '../utils/guards/user-address-info.guard';
 @Controller('user_addresses')
 export class UserAddressController {
   constructor(private readonly user_address_service: UserAddressService) {}
-  @UseGuards(AuthGuard(), UserAddressInfoGuard) // TODO: Create UserAddressInfoGuard to make sure user can't change write only information
+
+  /** GET all of the user addresses belonging to :user_uuid */
+  @Get(':user_uuid')
+  // Verify user is logged in, Verify user_uuid matches logged in user's auth token
+  @UseGuards(AuthGuard(), UserAccess)
+  @UseInterceptors(ClassSerializerInterceptor)
+  findByUserUUID(
+    @Param('user_uuid') user_uuid: string,
+  ): Promise<UserAddress[]> {
+    try {
+      return this.user_address_service.findByUserUUID(user_uuid);
+    } catch (ex) {
+      Logger.error(ex);
+      throw new InternalServerErrorException('Unknown error');
+    }
+  }
 
   /** Create a new user address */
   @Post()
