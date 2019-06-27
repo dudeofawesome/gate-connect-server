ae03e9d3e0321994e9f95d470679b4863714536e
Josh Gibbs <upaymeifixit@gmail.com>
2019-06-26 17:18:38 -0700

    💡 added documentation to UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -12,14 +12,17 @@ export class UserService {
     private readonly userRepository: Repository<User>,
   ) {}
 
+  /** Return all users in the database */
   findAll(): Promise<User[]> {
     return this.userRepository.find();
   }
 
+  /** Create user given partial user object */
   create(user: Partial<User>): Promise<User> {
     return this.userRepository.save<User>(this.userRepository.create(user));
   }
 
+  /** Find one or fail given user */
   async findOne(user: Partial<User>): Promise<User> {
     return this.userRepository.findOneOrFail({
       where: user,
@@ -36,32 +39,31 @@ export class UserService {
     });
   }
 
+  /** Find one user or fail given uuid */
   async findOneByUUID(uuid: string): Promise<User> {
     return this.userRepository.findOneOrFail({
       where: { uuid },
     });
   }
 
+  /** Update user's information based on uuid */
   async patch(uuid: string, user: Partial<User>): Promise<void> {
     await this.userRepository.update(uuid, user);
   }
 
-  // TODO: Shouldn't this be findByUserUUID and belong in GateGroupsService?
+  /** Return all the gate groups belonging to a user_uuid */
   async getGateGroups(user_uuid: string): Promise<GateGroup[]> {
-    return (
-      this.userRepository
-        .findOneOrFail({
-          where: { uuid: user_uuid },
-          relations: ['gate_groups', 'gate_groups.gates'],
-        })
-        // .then(user => user.gate_groups);
-        .then(user => {
-          const gate_groups: GateGroup[] = [];
-          user.addresses.forEach(user_address => {
-            gate_groups.push(user_address.gate_group_address.gate_group);
-          });
-          return gate_groups;
-        })
-    );
+    return this.userRepository
+      .findOneOrFail({
+        where: { uuid: user_uuid },
+        relations: ['gate_groups', 'gate_groups.gates'],
+      })
+      .then(user => {
+        const gate_groups: GateGroup[] = [];
+        user.addresses.forEach(user_address => {
+          gate_groups.push(user_address.gate_group_address.gate_group);
+        });
+        return gate_groups;
+      });
   }
 }
