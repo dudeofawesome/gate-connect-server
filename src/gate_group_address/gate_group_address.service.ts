86aa2010594d6d699b6790428e1b2ca5ea66db2e
Josh Gibbs <upaymeifixit@gmail.com>
2019-06-27 00:22:10 -0700

    ♻️ moved find linked addresses method

--- src/gate_group_address/gate_group_address.service.ts
+++ src/gate_group_address/gate_group_address.service.ts
@@ -13,55 +13,6 @@ export class GateGroupAddressService {
     private readonly gateGroupAddressRepository: Repository<GateGroupAddress>,
   ) {}
 
-  /** Return the closest GateGroupAddress to given UserAddress */
-  async find(user_address: Partial<UserAddress>): Promise<GateGroupAddress> {
-    const required_confidence = 0.7;
-    const gate_group_addresses = await this.findAll();
-    let best_gate_group_address;
-    let best_confidence = 0;
-    gate_group_addresses.forEach(gate_group_address => {
-      // We need to make sure ranges are always stored as 000a-999z
-      // Get premise range from GateGroupAddress
-      const range = gate_group_address.premise_range;
-      const min_premise = range.split('-')[0];
-      const max_premise = range.split('-')[1];
-      // Make sure zip code and state match, and address is within range
-      if (
-        user_address.postal_code === gate_group_address.postal_code &&
-        user_address.administrative_area ===
-          gate_group_address.administrative_area &&
-        user_address.premise != null &&
-        user_address.thoroughfare != null &&
-        user_address.locality != null &&
-        user_address.premise >= min_premise &&
-        user_address.premise <= max_premise
-      ) {
-        // Build street + city string and get a confidence level between them
-        const user_address_string =
-          user_address.thoroughfare + user_address.locality;
-        const gate_group_address_string =
-          gate_group_address.thoroughfare + gate_group_address.locality;
-        const confidence = compareTwoStrings(
-          user_address_string.replace(/\W/g, '').toLowerCase(),
-          gate_group_address_string.replace(/\W/g, '').toLowerCase(),
-        );
-        // Set best confidence if we find a better one
-        if (confidence > best_confidence) {
-          best_confidence = confidence;
-          best_gate_group_address = gate_group_address;
-        }
-      }
-    });
-    // The || is there for TypeScript
-    if (
-      best_confidence > required_confidence ||
-      best_gate_group_address == null
-    ) {
-      throw new NotFoundException();
-    }
-    return best_gate_group_address;
-  }
-
   findAll(): Promise<GateGroupAddress[]> {
     return this.gateGroupAddressRepository.find();
   }
