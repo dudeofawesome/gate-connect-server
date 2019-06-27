7cbd777e68eb50c9e2e286d8396505bc693b70d9
Louis Orleans <louis@orleans.io>
2019-06-26 22:57:08 -0700

    🐛 fix DI

--- src/user_address/user_address.module.ts
+++ src/user_address/user_address.module.ts
@@ -5,12 +5,14 @@ import { UserAddressService } from './user_address.service';
 import { UserAddress } from './user_address.entity';
 import { UserAddressController } from './user_address.controller';
 import { AuthModule, passportModule } from '../auth/auth.module';
+import { GateGroupAddressModule } from '../gate_group_address/gate_group_address.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserAddress]),
     passportModule,
     forwardRef(() => AuthModule),
+    GateGroupAddressModule,
   ],
   providers: [UserAddressService],
   controllers: [UserAddressController],
