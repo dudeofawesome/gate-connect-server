e07d4a58c5c9dba0fee08de6fa6187f6a4b2b584
Louis Orleans <louis@orleans.io>
2019-06-27 00:51:45 -0700

    🐛 fix DI

--- src/gate_group_address/gate_group_address.module.ts
+++ src/gate_group_address/gate_group_address.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { GateGroupAddressService, GateGroupAddress } from '.';
+import { UserAddressModule } from '../user_address/user_address.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GateGroupAddress])],
+  imports: [TypeOrmModule.forFeature([GateGroupAddress]), UserAddressModule],
   providers: [GateGroupAddressService],
   exports: [GateGroupAddressService],
 })
